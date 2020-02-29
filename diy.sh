3775fd8a8ef702cb01f82ccc64c988adeeabab59
xnxy2012 <35721438+xnxy2012@users.noreply.github.com>
2020-02-29 10:44:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,4 +7,4 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/jefferymvp/luci-app-koolproxyR package/
+git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
