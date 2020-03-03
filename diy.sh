2dadd11cae8c5f75f6ef47b30a0092496bab0106
xnxy2012 <35721438+xnxy2012@users.noreply.github.com>
2020-03-03 15:00:50 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,3 +1,4 @@
+  
 #!/bin/bash
 #=================================================
 # Description: DIY script
@@ -7,6 +8,10 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+#移除不用软件包
+cd openwrt/package/lean
+rm -rf k3screenctrl
+#添加额外软件包
 git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
 git clone https://github.com/yangsongli/luci-theme-atmaterial.git package/luci-theme-atmaterial
 git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/k3/luci-app-k3screenctrl
